99a325976f02c37d9830b8131f09f13a3489270f
Andrés García <playsharp59@gmail.com>
2022-03-28 17:45:27 +0200

    added styles to messages page

--- src/pages/MessagesPage/MessagesPage.jsx
+++ src/pages/MessagesPage/MessagesPage.jsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useState } from "react"
+import { Link } from 'react-router-dom'
 import FriendsChatWindow from "../../components/FriendsChatWindow/FriendsChatWindow"
 import userService from "../../services/user.service"
 import { AuthContext } from '../../context/auth.context'
@@ -8,36 +9,45 @@ const MessagesPage = () => {
     const { user } = useContext(AuthContext)
     const [userInfo, setUserInfo] = useState({})
     const [friendSelected, setFriendSelected] = useState([])
-    const [friendName, setFriendName] = useState('')
+    const [friendRaw, setFriendRaw] = useState('')
 
     useEffect(() => {
         userService
             .getOneUserById(user?._id)
             .then(({ data }) => {
                 setUserInfo(data)
-                const friendsList = data.friends?.map(eachFriend => userService.getOneUserById(eachFriend))
+                const friendsList = data.friends?.map(eachFriend => userService.getOneUserById(eachFriend).then(({ data }) => data))
                 return Promise.all(friendsList)
             })
-            .then(friends => console.log(friends)) // Mejorar la obtención de datos
+            .then(friends => setFriendSelected(friends))
             .catch(err => console.log(err))
     }, [user?._id])
 
-    // const friendRaw = friendSelected?.filter(elm => elm.data.imageURL === friendName)
-
-
+    const friend = friendSelected?.find(elm => elm.imageURL === friendRaw)
 
     return (
         <div className="messagesPage">
-            <div className="friendsListChat" onClick={(e) => setFriendName(e.target.parentElement.firstChild.getAttribute('src'))}>
+            <div className="friendsListChat" onClick={(e) => setFriendRaw(e.target.parentElement.firstChild.getAttribute('src'))}>
                 {
                     userInfo.friends?.map((eachFriend, idx) => <FriendsChatWindow key={idx} eachFriend={eachFriend} />)
                 }
             </div>
-            <div className="chatWindow">
-                {
-
-                }
-            </div>
+            {
+                friend !== undefined &&
+                <div className="chatWindow">
+                    <div className="friendInfoPage">
+                        <div>
+                            <img src={friend?.imageURL} alt={friend?.username} />
+                            <p>{friend?.nameUser} {friend?.surnameUser}</p>
+                        </div>
+                        <Link to={`/perfil/${friend?.username}`}>Ir al perfil</Link>
+                    </div>
+                    <div className="chatWindowMessages"></div>
+                    <div className="chatWindowInput">
+                        <input type="text" name="chat" placeholder="Envía un mensaje..." />
+                    </div>
+                </div>
+            }
         </div>
     )
 }
